93397b9b5af249452f1a3b818643430238493b97
Hersen1 <63982leo@universidaddeleon.edu.mx>
2024-12-03 12:24:19 -0600

    app.py

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ DB_PASSWORD = "B0mb3r0s.*2024"  # Contraseña del usuario
 # Función para conectar a la base de datos
 def get_db_connection():
     conn = psycopg2.connect(
-        host=DB_HOST,
-        database=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD
+        host=os.getenv('DB_HOST', 'localhost'),  # Usa la variable de entorno DB_HOST
+        database=os.getenv('DB_NAME', DB_NAME),
+        user=os.getenv('DB_USER', DB_USER),
+        password=os.getenv('DB_PASSWORD', DB_PASWORD)
     )
     return conn
 
